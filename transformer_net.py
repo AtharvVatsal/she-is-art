3d268f2bf38d3dffc13ee48605a00f3254b6c0b8
Atharv Vatsal <76103582+AtharvVatsal@users.noreply.github.com>
2025-07-27 19:01:09 +0530

    Update transformer_net.py

--- transformer_net.py
+++ transformer_net.py
@@ -12,12 +12,12 @@ class ConvLayer(nn.Module):
         self.conv2d = nn.Conv2d(in_c, out_c, k, stride)
 
     def forward(self, x):
-        return self.conv2d(self.reflection_pad(x))
+        x = self.reflection_pad(x)
+        return self.conv2d(x)
 
 class ResidualBlock(nn.Module):
     def __init__(self, channels):
         super().__init__()
-        # Turn on running stats to match old checkpoints
         self.conv1 = ConvLayer(channels, channels, 3, 1)
         self.in1   = nn.InstanceNorm2d(channels, affine=True, track_running_stats=True)
         self.conv2 = ConvLayer(channels, channels, 3, 1)
@@ -40,14 +40,16 @@ class UpsampleConvLayer(nn.Module):
 
     def forward(self, x):
         if self.upsample:
-            x = F.interpolate(x, scale_factor=self.upsample, mode='nearest')
-        return self.conv2d(self.reflection_pad(x))
+            # JIT requires scale_factor to be float or list of floats
+            x = F.interpolate(x, scale_factor=float(self.upsample), mode='nearest')
+        x = self.reflection_pad(x)
+        return self.conv2d(x)
 
 class TransformerNet(nn.Module):
     def __init__(self):
         super().__init__()
-        # Initial conv layers
-        self.conv1 = ConvLayer(3, 32, 9, 1)
+        # Initial convolution layers
+        self.conv1 = ConvLayer(3,  32, 9, 1)
         self.in1   = nn.InstanceNorm2d(32, affine=True, track_running_stats=True)
         self.conv2 = ConvLayer(32, 64, 3, 2)
         self.in2   = nn.InstanceNorm2d(64, affine=True, track_running_stats=True)
@@ -55,28 +57,29 @@ class TransformerNet(nn.Module):
         self.in3   = nn.InstanceNorm2d(128, affine=True, track_running_stats=True)
         self.relu  = nn.ReLU()
 
-        # Residuals
+        # Residual blocks
         self.res1 = ResidualBlock(128)
         self.res2 = ResidualBlock(128)
         self.res3 = ResidualBlock(128)
         self.res4 = ResidualBlock(128)
         self.res5 = ResidualBlock(128)
 
-        # Upsampling (must use deconv naming to match checkpoints)
+        # Upsampling layers (named to match the checkpoint keys)
         self.deconv1 = UpsampleConvLayer(128, 64, 3, 1, upsample=2)
         self.in4     = nn.InstanceNorm2d(64, affine=True, track_running_stats=True)
-        self.deconv2 = UpsampleConvLayer(64, 32, 3, 1, upsample=2)
+        self.deconv2 = UpsampleConvLayer(64,  32, 3, 1, upsample=2)
         self.in5     = nn.InstanceNorm2d(32, affine=True, track_running_stats=True)
-        self.deconv3 = ConvLayer(32, 3, 9, 1)
+        self.deconv3 = ConvLayer(32,  3, 9, 1)
 
     def forward(self, x):
         y = self.relu(self.in1(self.conv1(x)))
         y = self.relu(self.in2(self.conv2(y)))
         y = self.relu(self.in3(self.conv3(y)))
 
-        y = self.res1(y); y = self.res2(y); y = self.res3(y)
-        y = self.res4(y); y = self.res5(y)
+        y = self.res1(y); y = self.res2(y)
+        y = self.res3(y); y = self.res4(y); y = self.res5(y)
 
         y = self.relu(self.in4(self.deconv1(y)))
         y = self.relu(self.in5(self.deconv2(y)))
         return self.deconv3(y)
+    
